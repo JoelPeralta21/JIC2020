b7bcacaa40999ed36e286d102e7c8ac4e0a3f89f
Joel Peralta <63027817+JoelPeralta21@users.noreply.github.com>
2020-11-07 00:46:29 -0500

    Agregar archivos de proyecto.

---
+++ API/Controllers/HospitalController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ServiciodeDatos.Model;
+
+namespace API.Controllers
+{
+    public class HospitalController : ApiController
+    {
+        PruebaJIC2020Entities bd = new PruebaJIC2020Entities();
+
+        [HttpGet]
+        public IEnumerable<Hospital> Get()
+        {
+            var listado = bd.Hospital.ToList();
+            return listado;
+        }
+
+        [HttpGet]
+        public Hospital Get(int id)
+        {
+            var hospital = bd.Hospital.FirstOrDefault(x => x.IdHospital == id);
+            return hospital;
+        }
+    }
+}
---
+++ API/Controllers/MedicamentosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ServiciodeDatos.Model;
+
+namespace API.Controllers
+{
+    public class MedicamentosController : ApiController
+    {
+        PruebaJIC2020Entities bd = new PruebaJIC2020Entities();
+        [HttpGet]
+        public IEnumerable<Medicamentos> Get()
+        {
+            var listado = bd.Medicamentos.ToList();
+            return listado;
+        }
+
+        [HttpGet]
+        public Medicamentos Get(int id)
+        {
+            var medicamentos = bd.Medicamentos.FirstOrDefault(x => x.IdMedicamento == id);
+            return medicamentos;
+        }
+    }
+}
